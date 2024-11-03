3508f05c5221667dc6fa55d2f227975546960c01
Gabriel Lima <gabrielmartinslima@gmail.com>
2024-11-03 01:21:47 -0300

    SPI Master: Inheritance from SharedResource

--- Inc/SPI_Master.hpp
+++ Inc/SPI_Master.hpp
@@ -8,13 +8,11 @@
 #ifndef SRC_COMPONENTS_SPI_MASTER_HPP_
 #define SRC_COMPONENTS_SPI_MASTER_HPP_
 
-#include <inttypes.h>
+#include <SharedResource.hpp>
 
-class SPI_Master {
+class SPI_Master : public SharedResource {
 public:
 	virtual int32_t init() = 0;
-	virtual int32_t take(uint32_t timeout) = 0;
-	virtual int32_t give() = 0;
 	virtual int32_t transmit(uint8_t* buffer, uint32_t length) = 0;
 	virtual int32_t receive(uint8_t* buffer, uint32_t length) = 0;
 	virtual int32_t trx(uint8_t* txBuffer, uint8_t* rxBuffer, uint32_t length) = 0;

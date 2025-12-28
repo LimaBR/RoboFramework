1f1cbfba2bf6ed94a061382d8c9e13e293097d31
Gabriel Lima <gabrielmartinslima@gmail.com>
2025-12-27 23:56:49 -0300

    Add HIDRAW.hpp

---
+++ Inc/HIDRAW.hpp
@@ -0,0 +1,20 @@
+/*
+ * HIDRAW.hpp
+ *
+ *  Created on: Dec 27, 2025
+ *      Author: Gabriel
+ */
+
+#ifndef SHARED_ROBOFRAMEWORK_INC_HIDRAW_HPP_
+#define SHARED_ROBOFRAMEWORK_INC_HIDRAW_HPP_
+
+#include "SharedResource.hpp"
+
+class HIDRAW : public SharedResource {
+public:
+	virtual int32_t init() = 0;
+	virtual int32_t enqueueInPacket(uint8_t* buffer, uint32_t size) = 0;
+	virtual int32_t receiveOutPacket(uint8_t* buffer, uint32_t maxSize) = 0;
+};
+
+#endif /* SHARED_ROBOFRAMEWORK_INC_HIDRAW_HPP_ */
